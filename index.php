3151f92a2575911d10df9adbda64be0e07f0a6b3
Nguyenduyquy11 <quynguyenduy150@gmail.com>
2024-03-21 16:00:23 +0700

    abc

--- index.php
+++ index.php
@@ -3,7 +3,5 @@ session_start();
     require_once 'env.php';
     require_once 'vendor/autoload.php';
     require_once 'app/common/route.php';
-    echo "Nguyễn Duy Quý đẹp trai vãi ò";
-    echo "Anh Quy dep trai nhat Viet Nam";
-    echo "Anh quý không những đẹp trai lại còn tốt bụng";
+   
 ?>
